9b91c9ec05ef0733da1f00b0ba10147236d6b17a
Mahmudul Alam <mahmudula2000@gmail.com>
2021-07-06 09:54:38 +0600

    Removed unsupported code for python3.7

--- app.py
+++ app.py
@@ -15,33 +15,6 @@ def hello_world():
 DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 
 
-class JudgeTestCase:
-    def __init__(self, test_id, provided_input, expected_output=None, output_checker=None, on_complete=None):
-        if bool(expected_output) == bool(output_checker):
-            raise ValueError("expect output or output_checker, only one is required")
-        if output_checker and not callable(output_checker):
-            raise ValueError("Output checker must be function")
-        if on_complete and not callable(on_complete):
-            raise ValueError("On complete must be an function")
-        self.id = test_id
-        self.input = provided_input
-        self.expected_output = expected_output
-        self.output_checker = output_checker
-        self.on_complete = on_complete
-
-
-class JudgeData:
-    def __init__(self, judge_id, code, language, test_cases: list[JudgeTestCase], on_complete, time_limit=1,
-                 memory_limit=256):
-        self.id = judge_id
-        self.code = code
-        self.language = language
-        self.test_cases = test_cases
-        self.on_complete = on_complete
-        self.time_limit = time_limit
-        self.memory_limit = memory_limit
-
-
 def is_equal(words):
     word1, word2 = words
     if check_float(word1):
